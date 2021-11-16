ef81350886b1ca982731a11715634f15063fce07
Avec112 <avec112@gmail.com>
2021-11-16 12:44:09 +0100

    Changed to Vaadin Dark mode

--- src/main/java/io/avec/ced/Application.java
+++ src/main/java/io/avec/ced/Application.java
@@ -4,12 +4,13 @@ import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@Theme(value = "myapp")
+@Theme(value = "myapp", themeClass = Lumo.class, variant = Lumo.DARK)
 @PWA(name = "Superhero", shortName = "Superhero", offlineResources = {"images/logo.png"}, offlinePath = "offline.html")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
